ccad94f42a65b4ac8722cbfda3f17de33e632972
Pau iglesias <14270729b@iespoblenou.org>
2017-03-16 17:30:54 +0100

    Funcionalidad añadir partners completada

--- app/src/main/java/com/lafamilia/rim/app/PartnerActivityFragment.java
+++ app/src/main/java/com/lafamilia/rim/app/PartnerActivityFragment.java
@@ -7,6 +7,8 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
 
 import com.lafamilia.rim.R;
@@ -15,17 +17,18 @@ import com.lafamilia.rim.models.Partner;
 
 import io.realm.Realm;
 import io.realm.RealmConfiguration;
-import io.realm.RealmQuery;
 import io.realm.RealmResults;
 
 /**
  * A placeholder fragment containing a simple view.
  */
-public class PartnerActivityFragment extends Fragment {
+public class PartnerActivityFragment extends Fragment implements View.OnClickListener {
 
     private Realm realm;
     private PartnerAdapter partnerAdapter;
     private ListView listView;
+    private Button addPartnet;
+    private EditText name, age;
 
     public PartnerActivityFragment() {
     }
@@ -36,29 +39,16 @@ public class PartnerActivityFragment extends Fragment {
         initRealm();
     }
 
-    private void initRealm() {
-        // Initialize Realm
-        Realm.init(getContext());
-
-        // Configure Realm
-        RealmConfiguration realmConfiguration = new RealmConfiguration
-                .Builder()
-                .name("library.realm")
-                .build();
-
-        // Clear the realm from last time
-        Realm.deleteRealm(realmConfiguration);
-
-        // Create a new empty instance of Realm
-        realm = Realm.getInstance(realmConfiguration);
-    }
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_partner, container, false);
 
         listView = (ListView) view.findViewById(R.id.list_view_partner);
+
+        Button addPartner= (Button) view.findViewById(R.id.addPartner);
+
+        addPartner.setOnClickListener(this);
 
         return view;
     }
@@ -69,19 +59,35 @@ public class PartnerActivityFragment extends Fragment {
 
         // Set up adapter
         setAdapter();
+    }
+
+    public RealmResults<Partner> loadPartners() {
+        // Get a list of partners from the realm database
+        return realm.where(Partner.class).findAll();
+    }
+
+
+    @Override
+    public void onClick(View view) {
+
+        final String pName;
+        final int pAge;
+
+        pName = name.getText().toString();
+        pAge = Integer.valueOf(age.getText().toString());
+
+        if(pName.isEmpty() || pAge == 0)return;
 
         realm.executeTransactionAsync(new Realm.Transaction() {
             @Override
             public void execute(Realm bgRealm) {
                 // Save objects to realm database
-                Partner partner1 = bgRealm.createObject(Partner.class);
-                Partner partner2 = bgRealm.createObject(Partner.class);
+                Partner partner = bgRealm.createObject(Partner.class);
 
-                partner1.setName("Marc");
-                partner1.setAge(29);
 
-                partner2.setName("David");
-                partner2.setAge(19);
+                partner.setName(pName);
+                partner.setAge(pAge);
+
             }
         }, new Realm.Transaction.OnSuccess() {
             @Override
@@ -99,7 +105,26 @@ public class PartnerActivityFragment extends Fragment {
         });
     }
 
-    public RealmResults<Partner> loadPartners() {
+    private void initRealm() {
+        // Initialize Realm
+        Realm.init(getContext());
+
+        // Configure Realm
+        RealmConfiguration realmConfiguration = new RealmConfiguration
+                .Builder()
+                .name("library.realm")
+                .build();
+
+        // Clear the realm from last time
+//        Realm.deleteRealm(realmConfiguration);
+
+        // Create a new empty instance of Realm
+        realm = Realm.getInstance(realmConfiguration);
+    }
+
+
+
+    public RealmResults<Partner> lloadPartners() {
         // Get a list of partners from the realm database
         return realm.where(Partner.class).findAll();
     }
@@ -108,4 +133,5 @@ public class PartnerActivityFragment extends Fragment {
         partnerAdapter = new PartnerAdapter(loadPartners());
         listView.setAdapter(partnerAdapter);
     }
+
 }
