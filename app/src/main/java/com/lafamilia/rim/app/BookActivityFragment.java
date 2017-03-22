16435ea052073d88a8e50a4e23956e2e1a3de909
cristian <38853841x@iespoblenou.org>
2017-03-22 20:28:17 +0100

    Ok

--- app/src/main/java/com/lafamilia/rim/app/BookActivityFragment.java
+++ app/src/main/java/com/lafamilia/rim/app/BookActivityFragment.java
@@ -65,8 +65,7 @@ public class BookActivityFragment extends Fragment {
 //        listView.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
 //            @Override
 //            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-//                parent.getItemAtPosition(position)
-//
+//                Book book = loadBooks().get(position);
 //
 //            }
 //
